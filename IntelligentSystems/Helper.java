54d743e56c24f2fb20d26e3a8b35c7f1eac4be12
Baudouin Duthoit <baud.duthoit@gmail.com>
2014-01-20 19:02:00 +0100

    Comment in test fucntion all that is working

--- IntelligentSystems/Helper.java
+++ IntelligentSystems/Helper.java
@@ -61,51 +61,52 @@ public class Helper {
     
     // Testing function :
     public static void main(String[] args) {
-    	SimulatedPlanetWars simpw = fakePW();
-        // MyNode root = new MyNode(simpw);
-        // root.createSons();
-        // for (MyNode node : root.getSons())
-        // 	System.out.println(node);
-
-        // BeamsearchBot.DoTurn(simpw); => not working (cf signature of the function) :
-
-  		int testIndex = 0;
-		Planet source = null;
-		Planet dest = null;
-
-		MyNode root = new MyNode(simpw);
-
-		ArrayList<MyNode> beam = new ArrayList<MyNode>(3);
-		beam.add(root);
+		SimulatedPlanetWars simpw = fakePW();
+		// // MyNode class tests :
+		// MyNode root = new MyNode(simpw);
+		// root.createSons();
 		// for (MyNode node : root.getSons())
-		// 	System.out.println("I'm a node of " + node);
+		// 	System.out.println(node);
 
-		source = root.getSource();
-		dest = root.getDest();
-		//While there is still some time, we go through the tree of possibilities
-		while(testIndex < 100){
-			for (int i = 0; i<beam.size();++i){
-				MyNode node = beam.get(i);
-				beam.remove(i);		//When it has been treated, we take it off the list
-				node.createSons();
-				System.out.println(node);
-				for (MyNode son : node.getSons()){
-					son.conditionnalAdd(3,beam);
-				}
-			}
-			++ testIndex;
-		}
+		// // BeamsearchBot.DoTurn(simpw); => not working (cf signature of the function) :
 
-		// Choosing the maximum value 
+		// int testIndex = 0;
+		// Planet source = null;
+		// Planet dest = null;
 
-		int max = beam.get(0).getValue();
-		int index = 0;
-		for (int i = 1;i<beam.size();++i){
-			MyNode node = beam.get(i);
-			if (node.getValue()>max) {
-				index = i;
-				max = node.getValue();
-			}
-		}
+		// MyNode root = new MyNode(simpw);
+
+		// ArrayList<MyNode> beam = new ArrayList<MyNode>(3);
+		// beam.add(root);
+		// // for (MyNode node : root.getSons())
+		// // 	System.out.println("I'm a node of " + node);
+
+		// source = root.getSource();
+		// dest = root.getDest();
+		// //While there is still some time, we go through the tree of possibilities
+		// while(testIndex < 100){
+		// 	for (int i = 0; i<beam.size();++i){
+		// 		MyNode node = beam.get(i);
+		// 		beam.remove(i);		//When it has been treated, we take it off the list
+		// 		node.createSons();
+		// 		System.out.println(node);
+		// 		for (MyNode son : node.getSons()){
+		// 			son.conditionnalAdd(3,beam);
+		// 		}
+		// 	}
+		// 	++ testIndex;
+		// }
+
+		// // Choosing the maximum value 
+
+		// int max = beam.get(0).getValue();
+		// int index = 0;
+		// for (int i = 1;i<beam.size();++i){
+		// 	MyNode node = beam.get(i);
+		// 	if (node.getValue()>max) {
+		// 		index = i;
+		// 		max = node.getValue();
+		// 	}
+		// }
     }
 }
